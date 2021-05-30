e357b66c29ba9b98be59811abb887df963feb5b5
pranav kulkarni <pranav.kulkarni54@gmail.com>
2021-05-30 13:00:51 +0530

    Removed unnecessary data

--- Backend/visualizer/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from django.contrib import messages
-
-class InputForm(forms.Form):
-    query = forms.CharField(max_length=200)
--- Backend/visualizer/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from visualizer import views
-
-urlpatterns = [
-    path('', views.fire, name='fire'),
-]
